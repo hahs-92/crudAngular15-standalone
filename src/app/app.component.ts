d8f769172387e2dfc9c6c14fcac5ecd3a8b13b75
hugo.hernandez <hugo.hernandez@sofka.com.co>
2022-12-23 14:45:17 -0500

    initial config: standalone app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // para trabajar con standalone
+  standalone: true,
+  //add los modules necesarios
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'crud15-angular-standalone';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+//! Eliminamos el appModule y app-routing
+// import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// para hacer nuestra app syandalone
+bootstrapApplication(AppComponent);
