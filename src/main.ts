0d7f0bc932fd0be238668b9ef1f2a8ee623544be
hugo.hernandez <hugo.hernandez@sofka.com.co>
2022-12-23 15:19:56 -0500

    added: users routes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 // import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // angular/fire library
@@ -8,8 +9,9 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 //! Eliminamos el appModule y app-routing
 // import { AppModule } from './app/app.module';
-import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
 import { enviroment } from './enviroments/enviroment';
+import { AppComponent } from './app/app.component';
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
@@ -17,9 +19,10 @@ import { enviroment } from './enviroments/enviroment';
 // para hacer nuestra app syandalone
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(appRoutes),
     importProvidersFrom(
       provideFirebaseApp(() => initializeApp(enviroment.firebase)),
       provideFirestore(() => getFirestore())
     ),
   ],
-});
+}).catch(console.error);
