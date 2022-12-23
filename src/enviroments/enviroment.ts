dcde56cf50ef40b161aa0aefb796c3b3e751852d
hugo.hernandez <hugo.hernandez@sofka.com.co>
2022-12-23 15:04:18 -0500

    added: firebase config

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+//! este archivo lo agregamos manualmente
+
+export const enviroment = {
+  firebase: {
+    apiKey: 'AIzaSyAwVHEKs8umEdU7vvHs-UxlA2VXc65FW2k',
+    authDomain: 'crudangular15-bcd00.firebaseapp.com',
+    projectId: 'crudangular15-bcd00',
+    storageBucket: 'crudangular15-bcd00.appspot.com',
+    messagingSenderId: '529974186860',
+    appId: '1:529974186860:web:b0aba8493148a2c34b9949',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 // import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// angular/fire library
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 //! Eliminamos el appModule y app-routing
 // import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
+import { enviroment } from './enviroments/enviroment';
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
 // para hacer nuestra app syandalone
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(enviroment.firebase)),
+      provideFirestore(() => getFirestore())
+    ),
+  ],
+});
